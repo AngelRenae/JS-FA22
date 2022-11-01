b7f5f7e891fdd9e81c8342ab0f1a9dcbdbd5f9a1
Angel Thomas <thomas.angel489@gmail.com>
2022-11-01 00:54:37 -0700

    day one
    general introduction, querySelector

---
+++ day-1/js/main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+document.querySelector('button').innerText = 'hi';
+
+document.querySelector('.btn-1').innerText = 'UH';
+
+document.querySelector('#myButton').innerText = 'nah';
+
+document.querySelector('button').style.background = 'blue';
+
+document.querySelector('.btn-1').style.color = 'white';
+
+document.querySelector('#myButton').style.fontStyle = 'italic';
+
+document.querySelector.addEventListener('click', () => {
+    
+})
